71cc3f8a7d522665985490f39a43f8797683566e
Margarita <margomimi82@gmail.com>
2020-10-23 19:58:00 +0300

    redesign site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 
 (function($){
 	$(document).ready(function() {
-		// Code
+		let menu = $('.navigation');
+		$(window).scroll(function(){
+			if ( $(this).scrollTop() > 94 ){
+			$(menu).addClass("fixed");
+				} else if($(this).scrollTop() <= 94) {
+						$(menu).removeClass("fixed");
+				}
+			});
 
 	});
 })(jQuery);
