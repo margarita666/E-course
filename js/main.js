0209283faf7bc6e33f1145d5af796e70177f9e11
margarita666_ <mchernysh@morebis.net>
2020-11-19 15:50:17 +0200

    quiz funtionality added

--- js/main.js
+++ js/main.js
@@ -43,13 +43,21 @@
 })(jQuery);
 
 function submitAnswers() {
-  var total = 3;
+  var total = 10;
   var score = 0;
   
   //Get user input
   var q1 = document.forms['quizForm']['q1'].value;
   var q2 = document.forms['quizForm']['q2'].value;
   var q3 = document.forms['quizForm']['q3'].value;
+  var q4 = document.forms['quizForm']['q4'].value;
+  var q5 = document.forms['quizForm']['q5'].value;
+  var q6 = document.forms['quizForm']['q6'].value;
+  var q7 = document.forms['quizForm']['q7'].value;
+  var q8 = document.forms['quizForm']['q8'].value;
+  var q9 = document.forms['quizForm']['q9'].value;
+  var q10 = document.forms['quizForm']['q10'].value;
+
   
   // Validation  
   for(var i = 1; i <= total; i++) {
@@ -60,7 +68,7 @@ function submitAnswers() {
   }
   
   // Set correct answers
-  var answers = ["b","d","b"];
+  var answers = ["c","b","a","c","d","c","b","d","b","c"];
   
   for(var i = 1; i <= total; i++) {
     // Check answers
